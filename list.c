51e7b5044ec2746c9ac35e9d67683a743e42a25d
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-07 03:11:44 -0800

    code that makes betty happy

---
+++ list.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: 0 for success.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
