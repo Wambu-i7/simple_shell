c9b332528423acc61b2c06b7951b581266b1ab58
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-15 06:31:59 -0800

    Adding prototypes

--- shell.h
+++ shell.h
@@ -6,13 +6,36 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-void execute_command(char *command);
-void tokenization(char *input);
-void read_line(void);
+#include <stdarg.h>
+#include <sys/stat.h>
+
 #define MAX_ARGS 64
 #define MAX_LENGTH 1024
 #define EXTRA_SPACE 2
 #define PATH_LENGTH 5
+int _putchar(char c);
+
 void environ(void);
-char *concat_path_cmd(char *path, char *c);
+
+void exit_prog(char **c, char *input);
+char *valid_path(char **path, char *c);
+char *concat_path_cmd(char *path, char *c)
+int builtin_func(char **c, char *input);
+void execute_command(char *c, char **args);
+void tokenization(char *input);
+void read_line(void);
+
+/**
+ * struct builtin builtin_struc - check for builtins.
+ * @env: Environment variable.
+ * @exit: exit the program.
+ *Return: Nothing.
+ */
+struct builtin
+{
+	char *env;
+	char *exit;
+} builtin_struct;
+
+/* Handle strings */
 #endif
