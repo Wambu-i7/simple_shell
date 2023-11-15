d58eb2e6597402b7216b53243323be929fe66bc0
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-15 11:26:06 -0800

    main to the shell

--- main.c
+++ main.c
@@ -1,11 +1,50 @@
 #include "shell.h"
 /**
-*main - Entry point.
-*Return: 0 indicating success.
-*/
-void read_line(void);
-int main(void)
-{
-	read_line();
-	return (0);
+ * main - Entry point.
+ * Return: Always 0 on success.
+ */
+
+int main(void) {
+    char *input;
+    char **tokens;
+    char *path;
+    char *command;
+
+    while (1) {
+        // Read input
+        read_line(&input);
+
+        // Tokenize input
+        tokens = tokenization(input);
+
+        // Check for built-in commands
+        if (builtin_func(tokens, input)) {
+            // If it's a built-in command, continue to the next iteration of the loop
+            free(input);
+            free(tokens);
+            continue;
+        }
+
+        // Find the PATH
+        path = find_path();
+
+        // Validate the path and get the full command path
+        command = valid_path(path, tokens[0]);
+
+        if (command != NULL) {
+            // Execute the command
+            execute_command(command, tokens);
+            free(command);
+        } else {
+            // Handle invalid command
+            fprintf(stderr, "Command not found: %s\n", tokens[0]);
+        }
+
+        // Free allocated memory
+        free(input);
+        free(tokens);
+    }
+
+    return 0;
 }
+
