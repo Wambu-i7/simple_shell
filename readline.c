c9ec8c63ad09b2c11a0572bd86e3cd3d07d5cdd3
Emmiechege <emilywanjachege@gmail.com>
2023-11-15 10:32:56 -0800

    A program that reads a line from standard input

--- readline.c
+++ readline.c
@@ -10,13 +10,15 @@ void read_line(void)
 	ssize_t read;/*nuber of characters read*/
 	const char *prompt;/*prompt string to be displayed*/
 
-	while (1)
+	while (1)/*infinite loop*/
 	{
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
 	{
 	prompt = "#cisfun$ ";
+	/*display prompt to user*/
 	write(STDOUT_FILENO, prompt, string_len(prompt));
 	}
+	/*read a line from std input*/
 	read = getline(&input, &len, stdin);
 	if (read == -1)
 	{
