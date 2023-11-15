92b8cf347d2dbd569d8d55463c357fa6f7f49d54
Wambu-i7 <wambuindikiru@gmail.com>
2023-11-15 04:25:03 -0800

    Checks whether path is valid

---
+++ valid_path.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * valid_path - checks valididty of the path.
+ * @path: Path to verify.
+ * @c: Command enterd by user.
+ * Return: path appended with a command on success
+ *  or NULL on failure.
+ */
+char *valid_path(char **path, char *c)
+{
+	int j = 0;
+	char *result;
+
+	while (path[j])
+	{
+		result = concat-path_cmd(path[j], c);
+		if (access(result, F_OK | X_OK) == 0)
+			return (result);
+		free(result);
+		j++;
+	}
+	return (NULL);
+}
