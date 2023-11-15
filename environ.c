5a169720b4951dd951e26ee9b46e9d762e831404
Emmiechege <emilywanjachege@gmail.com>
2023-11-15 00:22:27 -0800

    handles environment variables

---
+++ environ.c
@@ -0,0 +1,18 @@
+#include "shell.h"
+/**
+ * environ - handles the environment variables
+ * 
+ * Return: 0
+ */
+void environ(void)
+{
+	int i = 0;
+	char **env = envp
+
+	while (env[i] =! NULL)
+	{
+		write(STDOUT_FILENO, (const void *)env[i], string_len(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
