ef945186eba787b69f7f0cb15d675377f1ee3fe6
Emmiechege <emilywanjachege@gmail.com>
2023-11-15 14:03:05 -0800

    reads a line from user input

--- readline.c
+++ readline.c
@@ -12,28 +12,34 @@ void read_line(void)
 
 	while (1)/*infinite loop*/
 	{
-	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
-	{
-	prompt = "#cisfun$ ";
+		if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
+		{
+			prompt = "#cisfun$ ";
 	/*display prompt to user*/
-	write(STDOUT_FILENO, prompt, string_len(prompt));
-	}
-	/*read a line from std input*/
-	read = getline(&input, &len, stdin);
-	if (read == -1)
-	{
-		/*if end of file is reached,exit the loop*/
-		if (feof(stdin))
-		{
-			break;
+			write(STDOUT_FILENO, prompt, string_len(prompt));
 		}
-		else
+	/*read a line from std input*/
+		read = getline(&input, &len, stdin);
+		if (read == -1)
 		{
+		/*if end of file is reached,exit the loop*/
+			if (feof(stdin))
+			{
+				break;
+			}
+			else
+			{
 		/*if there is an erroe, print eror message*/
 			perror("getline");
+			free(input);/*free allocated memory*/
+			input = NULL;
+			len = 0;
 			break;
+
+			}
 		}
-	free(input);/*free space after using*/
+	}
+	free(input);/*free memory after loop*/
 	input = NULL;/*set pointer to null*/
 	len = 0;/*reset the length to 0*/
 }
