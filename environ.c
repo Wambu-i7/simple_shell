f072d7587428edb562d258697d7b0c66bd8d8f05
Emmiechege <emilywanjachege@gmail.com>
2023-11-15 00:54:26 -0800

    prints environment variables to stdout

--- environ.c
+++ environ.c
@@ -1,18 +1,20 @@
 #include "shell.h"
 /**
- * environ - handles the environment variables
+ * environ - handles the environment variables and prints them to stdout
  * 
- * Return: 0
+ * Return: has no return value
  */
 void environ(void)
 {
 	int i = 0;
-	char **env = envp
-
+	char **env = envp/*a pointer to environment variables*/
+/*loop through the variables*/
 	while (env[i] =! NULL)
 	{
+		/*writing content of current variable to stdout*/
 		write(STDOUT_FILENO, (const void *)env[i], string_len(env[i]));
+		/*add a newline to separate variables*/
 		write(STDOUT_FILENO, "\n", 1);
-		i++;
+		i++;/*move to next variable*/
 	}
 }
